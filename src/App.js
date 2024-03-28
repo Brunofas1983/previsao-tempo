f8e82496c0839c73694b39dd8416834de40d23d3
Bruno <brunoamarante83@gmail.com>
2024-03-28 12:34:12 -0300

    alteração no app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [cidade, setCidade] = useState('');
+  const [dadosTempo, setDadosTempo] = useState(null);
+  const [erro, setErro] = useState(null);
+
+  const obterPrevisaoTempo = async () => {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=27bcd6d0a505c68c9ef6cc504712ad6a&lang=pt_br&units=metric`);
+      setDadosTempo(response.data);
+      setErro(null);
+    } catch (error) {
+      setDadosTempo(null);
+      setErro('Cidade não encontrada. Por favor, tente novamente.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Previsão do Tempo</h1>
+      <input
+        type="text"
+        value={cidade}
+        onChange={(e) => setCidade(e.target.value)}
+        placeholder="Digite o nome da cidade"
+      />
+      <button className='btn-previsao' onClick={obterPrevisaoTempo}>Buscar</button>
+      {erro && <p>{erro}</p>}
+      {dadosTempo && (
+        <div className='resultado-previsao'>
+          <h2>{dadosTempo.name}</h2>
+          <p>{dadosTempo.weather[0].description}</p>
+          <p>Temperatura: {dadosTempo.main.temp}°C</p>
+          <p>Umidade: {dadosTempo.main.humidity}%</p>
+        </div>
+      )}
     </div>
   );
 }
